1dcd74e19e023c57fa6fc0a89eada896eea85392
alexiy <alexiy.ov@gmail.com>
2021-05-24 17:56:40 +0300

    Update shape

--- src/main/java/dev/buildtool/satako/blocks/BlockConnectable.java
+++ src/main/java/dev/buildtool/satako/blocks/BlockConnectable.java
@@ -5,15 +5,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SixWayBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 
 import javax.annotation.Nullable;
 
 public abstract class BlockConnectable extends SixWayBlock {
     public BlockConnectable(float thickness, Properties properties) {
         super(thickness, properties);
-        assert thickness < 1;
+        assert thickness <= 0.5;
         registerDefaultState(defaultBlockState().setValue(NORTH, false).setValue(SOUTH, false).setValue(UP, false).setValue(DOWN, false).setValue(EAST, false).setValue(WEST, false));
     }
 
@@ -35,4 +37,9 @@ public abstract class BlockConnectable extends SixWayBlock {
      * @return blockstate with appropriate connections
      */
     protected abstract BlockState makeConnections(IBlockReader blockReader, BlockPos pos);
+
+    @Override
+    public BlockState updateShape(BlockState blockState, Direction direction, BlockState state, IWorld world, BlockPos blockPos, BlockPos pos) {
+        return blockState.setValue(PROPERTY_BY_DIRECTION.get(direction), state.getBlock() == this);
+    }
 }
