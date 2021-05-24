83ccc0b0292c0546e59cd46d922b7bc19a423815
alexiy <alexiy.ov@gmail.com>
2021-05-24 18:56:26 +0300

    More boilerplate

--- src/main/java/dev/buildtool/satako/blocks/BlockConnectable.java
+++ src/main/java/dev/buildtool/satako/blocks/BlockConnectable.java
@@ -11,6 +11,8 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class BlockConnectable extends SixWayBlock {
     public BlockConnectable(float thickness, Properties properties) {
@@ -38,8 +40,29 @@ public abstract class BlockConnectable extends SixWayBlock {
      */
     protected abstract BlockState makeConnections(IBlockReader blockReader, BlockPos pos);
 
+    protected boolean doConnectTo(IWorld world, BlockState blockState, BlockState to, BlockPos pos, BlockPos toPos, Direction direction) {
+        return to.getBlock() == this;
+    }
+
     @Override
-    public BlockState updateShape(BlockState blockState, Direction direction, BlockState state, IWorld world, BlockPos blockPos, BlockPos pos) {
-        return blockState.setValue(PROPERTY_BY_DIRECTION.get(direction), state.getBlock() == this);
+    public BlockState updateShape(BlockState blockState, Direction on, BlockState state, IWorld world, BlockPos blockPos, BlockPos pos) {
+        return blockState.setValue(PROPERTY_BY_DIRECTION.get(on), doConnectTo(world, blockState, state, blockPos, pos, on));
+    }
+
+    public List<Direction> getConnections(BlockState blockState) {
+        ArrayList<Direction> directions = new ArrayList<>(6);
+        if (blockState.getValue(UP))
+            directions.add(Direction.UP);
+        if (blockState.getValue(DOWN))
+            directions.add(Direction.DOWN);
+        if (blockState.getValue(SOUTH))
+            directions.add(Direction.SOUTH);
+        if (blockState.getValue(NORTH))
+            directions.add(Direction.NORTH);
+        if (blockState.getValue(WEST))
+            directions.add(Direction.WEST);
+        if (blockState.getValue(EAST))
+            directions.add(Direction.EAST);
+        return directions;
     }
 }
