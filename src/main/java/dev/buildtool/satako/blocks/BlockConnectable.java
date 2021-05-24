38d0144617720b0e42ce1fc8160fd7b2f202abae
alexiy <alexiy.ov@gmail.com>
2021-05-24 12:16:57 +0300

    Abstract connection method

--- src/main/java/dev/buildtool/satako/blocks/BlockConnectable.java
+++ src/main/java/dev/buildtool/satako/blocks/BlockConnectable.java
@@ -3,9 +3,14 @@ package dev.buildtool.satako.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SixWayBlock;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 
-public class BlockConnectable extends SixWayBlock {
+import javax.annotation.Nullable;
+
+public abstract class BlockConnectable extends SixWayBlock {
     public BlockConnectable(float thickness, Properties properties) {
         super(thickness, properties);
         registerDefaultState(defaultBlockState().setValue(NORTH, false).setValue(SOUTH, false).setValue(UP, false).setValue(DOWN, false).setValue(EAST, false).setValue(WEST, false));
@@ -16,4 +21,17 @@ public class BlockConnectable extends SixWayBlock {
         super.createBlockStateDefinition(blockStateBuilder);
         blockStateBuilder.add(NORTH, SOUTH, EAST, WEST, UP, DOWN);
     }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext useContext) {
+        return makeConnections(useContext.getLevel(), useContext.getClickedPos());
+    }
+
+    /**
+     * Define block connections at given positions
+     *
+     * @return blockstate with appropriate connections
+     */
+    protected abstract BlockState makeConnections(IBlockReader blockReader, BlockPos pos);
 }
