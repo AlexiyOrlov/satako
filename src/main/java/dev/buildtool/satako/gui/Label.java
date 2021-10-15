8c8a8969ec309e6caee07926107264d86eb60328
alexiy <alexiy.ov@gmail.com>
2021-10-15 16:01:58 +0300

    Make label scrollable

--- src/main/java/dev/buildtool/satako/gui/Label.java
+++ src/main/java/dev/buildtool/satako/gui/Label.java
@@ -9,11 +9,14 @@ import net.minecraft.util.text.ITextComponent;
 /**
  * Label is a string for use in GUIs
  */
-public class Label extends Button
-{
+public class Label extends Button implements Scrollable {
+    protected boolean enabled, verticalScroll, horizontalScroll;
+    protected int scrollAmount;
+
     @SuppressWarnings("ConstantConditions")
     public Label(int x, int y, ITextComponent text) {
-        super(x,y,Minecraft.getInstance().font.width(text.getString())+8, 18, text,null);
+        super(x, y, Minecraft.getInstance().font.width(text.getString()) + 8, 18, text, null);
+        scrollAmount = height;
     }
 
     @Override
@@ -23,7 +26,39 @@ public class Label extends Button
 
 
     @Override
-    public void renderButton(MatrixStack matrixStack,int p_renderButton_1_, int p_renderButton_2_, float p_renderButton_3_) {
-        drawString(matrixStack,Minecraft.getInstance().font,this.getMessage(), this.x, this.y + (this.height - 8) / 2, 16777215 | MathHelper.ceil(this.alpha * 255.0F) << 24);
+    public void renderButton(MatrixStack matrixStack, int p_renderButton_1_, int p_renderButton_2_, float p_renderButton_3_) {
+        drawString(matrixStack, Minecraft.getInstance().font, this.getMessage(), this.x, this.y + (this.height - 8) / 2, 16777215 | MathHelper.ceil(this.alpha * 255.0F) << 24);
+    }
+
+    @Override
+    public void scroll(int amount, boolean vertical) {
+        if (vertical && verticalScroll) {
+            y += Math.signum(amount) * scrollAmount;
+        } else if (!vertical && horizontalScroll) {
+            x += amount;
+        }
+    }
+
+    @Override
+    public void setScrollable(boolean vertical, boolean b) {
+        if (vertical)
+            verticalScroll = b;
+        else
+            horizontalScroll = b;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public void setEnabled() {
+        enabled = true;
+    }
+
+    @Override
+    public void setDisabled() {
+        enabled = false;
     }
 }
