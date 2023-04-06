c7d8296f128520a41bf8d754863e48722ce23bff
alexiy <alexiy.ov@gmail.com>
2023-04-06 08:46:30 +0300

    Add string split function

--- src/main/java/dev/buildtool/satako/Functions.java
+++ src/main/java/dev/buildtool/satako/Functions.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -1090,4 +1091,50 @@ public final class Functions
         return new PacketBuffer(Unpooled.buffer());
     }
 
+    /**
+     * Splits every line so it fits the max width parameter
+     */
+    private List<String> splitString(List<String> strings, List<String> returnList, int nextIndex, FontRenderer font, int maxWidth) {
+        String string = strings.get(nextIndex);
+        if (font.width(string) > maxWidth) {
+            String part = string.substring(0, string.lastIndexOf(' '));
+            while (font.width(part) > maxWidth) {
+                part = part.substring(0, part.length() - 2);
+            }
+            while (!part.endsWith(" ")) {
+                part = part.substring(0, part.length() - 2);
+            }
+            String part2 = string.substring(part.length());
+            returnList.add(part);
+            if (font.width(part2) <= maxWidth) {
+                int next = nextIndex + 1;
+                if (next < strings.size()) {
+                    String nextString = strings.get(next);
+                    if (font.width(part2 + nextString) <= maxWidth) {
+                        returnList.add(part2 + nextString);
+                        if (next + 1 < strings.size())
+                            splitString(strings, returnList, next + 1, font, maxWidth);
+                    } else returnList.add(part2);
+                } else {
+                    returnList.add(part2);
+                }
+            }
+            int indexOfNext = strings.indexOf(string) + 1;
+            if (indexOfNext < strings.size()) {
+                splitString(strings, returnList, indexOfNext, font, maxWidth);
+            }
+        } else {
+            if (string.isEmpty()) {
+                splitString(strings, returnList, nextIndex + 1, font, maxWidth);
+            } else {
+                returnList.add(string);
+                int indexOfNext = strings.indexOf(string) + 1;
+                if (indexOfNext < strings.size()) {
+                    splitString(strings, returnList, indexOfNext, font, maxWidth);
+                }
+            }
+        }
+
+        return returnList;
+    }
 }
