fab37390333071893dc26d316c53963fd5a3e29f
alexiy <alexiy.ov@gmail.com>
2023-10-01 12:25:20 +0300

    Split client methods

--- src/main/java/dev/buildtool/satako/Methods.java
+++ src/main/java/dev/buildtool/satako/Methods.java
@@ -1,14 +1,9 @@
 package dev.buildtool.satako;
 
-import com.mojang.blaze3d.vertex.*;
-import com.mojang.math.Matrix4f;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
@@ -18,8 +13,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.items.IItemHandler;
 
 import java.lang.reflect.Field;
@@ -78,44 +71,6 @@ public final class Methods {
         }
     }
 
-    private static void drawCircle(Tesselator tessellator) {
-        BufferBuilder bufferbuilder = tessellator.getBuilder();
-        bufferbuilder.begin(VertexFormat.Mode.LINES, DefaultVertexFormat.POSITION_COLOR);
-        int num_segments = 16;
-        float radius = 0.7f;
-        for (int ii = 0; ii < num_segments; ii++) {
-            float theta = 2.0f * 3.1415926f * ii / num_segments;//get the current angle
-
-            float xx = radius * Mth.cos(theta);
-            float yy = radius * Mth.sin(theta);
-            bufferbuilder.vertex(xx, yy, 0).color(0, 0, 0, 255).endVertex();
-        }
-        tessellator.end();
-    }
-
-    private static void drawFilledCircle(Tesselator tessellator, float radius, IntegerColor color) {
-        int circle_points = 50;
-        float angle = 2.0f * 3.1416f / circle_points;
-        BufferBuilder bufferBuilder = tessellator.getBuilder();
-        //TODO check format
-        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
-        double angle1 = 0.0;
-        double xx = radius * Math.cos(0);
-        double yy = radius * Math.sin(0);
-        bufferBuilder.vertex(xx, yy, 0).color(color.getRed(), color.getGreen(), color.getBlue(), 255).endVertex();
-        int i;
-        for (i = 0; i < circle_points; i++) {
-            bufferBuilder.vertex(radius * Math.cos(angle1), radius * Math.sin(angle1), 0).color(0, 0, 0, 255).endVertex();
-            angle1 += angle;
-        }
-        tessellator.end();
-    }
-
-    public static void drawStringWithBackground(PoseStack matrixStack, Object obj, int x, int y, IntegerColor background) {
-        String string = obj.toString();
-        Minecraft.getInstance().font.draw(matrixStack, string, x + 2, y + 4, background.getIntColor());
-    }
-
     /**
      * Notifies clients of a block update
      */
@@ -124,43 +79,12 @@ public final class Methods {
         worldServer.sendBlockUpdated(blockPos, blockState, blockState, 2);
     }
 
-    @OnlyIn(Dist.CLIENT)
-    public static void openClientGui(Level world, Screen screen) {
-        if (world.isClientSide)
-            Minecraft.getInstance().setScreen(screen);
-    }
-
     public static void playSound(Level world, BlockPos blockPos, SoundEvent sound, float volume, float pitch) {
         world.playLocalSound(blockPos.getX(), blockPos.getY(), blockPos.getZ(), sound, null, volume, pitch, false);
     }
 
     public static void sendMessageToPlayer(Player player, String message) {
-        player.displayClientMessage(Component.literal(message), false);
-    }
-
-    /**
-     * @param X position to be centered on
-     */
-    public static void drawCenteredString(PoseStack matrixStack, net.minecraft.network.chat.Component o, int X, int Y, IntegerColor color) {
-        drawString(matrixStack, o, X - Functions.calculateStringWidth(o) / 2, Y, color);
-    }
-
-    /**
-     * @param X position to be centered on
-     */
-    public static void drawCenteredStringWithShadow(PoseStack matrixStack, Component o, int X, int Y, IntegerColor color) {
-        drawStringWithShadow(matrixStack, o, X - Functions.calculateStringWidth(o) / 2, Y, color);
-    }
-
-    /**
-     * Draws string without shadow
-     */
-    public static void drawString(PoseStack matrixStack, Object o, int X, int Y, IntegerColor color) {
-        Minecraft.getInstance().font.draw(matrixStack, o.toString(), X, Y, color.getIntColor());
-    }
-
-    public static void drawStringWithShadow(PoseStack matrixStack, Object o, int X, int Y, IntegerColor color) {
-        Minecraft.getInstance().font.draw(matrixStack, o.toString(), X, Y, color.getIntColor());
+        player.displayClientMessage(new TextComponent(message), false);
     }
 
     /**
@@ -209,81 +133,4 @@ public final class Methods {
         }
     }
 
-    /**
-     * @param addBackFaces whether to add back faces for the sides
-     * @param extruder     offsets faces
-     */
-    public static void addRectangle(VertexConsumer vertexConsumer, Matrix4f matrix4f, int width, int height, int depth, float red, float green, float blue, float alpha, boolean addBackFaces, float extruder) {
-        //Up
-        vertexConsumer.vertex(matrix4f, 0, height + 1 + extruder, 0).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, 0, height + 1 + extruder, depth + 1).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, width + 1, height + 1 + extruder, depth + 1).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, width + 1, height + 1 + extruder, 0).color(red, green, blue, alpha).endVertex();
-        if (addBackFaces) {
-            vertexConsumer.vertex(matrix4f, 1 + width, height + 1 + extruder, 0).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 1 + width, height + 1 + extruder, 1 + depth).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 0, height + 1 + extruder, 1 + depth).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 0, height + 1 + extruder, 0).color(red, green, blue, alpha).endVertex();
-        }
-
-        //Down
-        vertexConsumer.vertex(matrix4f, 1 + width, -extruder, 0).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, 1 + width, -extruder, 1 + depth).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, 0, -extruder, 1 + depth).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, 0, -extruder, 0).color(red, green, blue, alpha).endVertex();
-        if (addBackFaces) {
-            vertexConsumer.vertex(matrix4f, 0, -extruder, 0).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 0, -extruder, 1 + depth).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 1 + width, -extruder, 1 + depth).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 1 + width, -extruder, 0).color(red, green, blue, alpha).endVertex();
-        }
-
-        //North
-        vertexConsumer.vertex(matrix4f, 0, 0, -extruder).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, 0, height + 1, -extruder).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, width + 1, height + 1, -extruder).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, width + 1, 0, -extruder).color(red, green, blue, alpha).endVertex();
-        if (addBackFaces) {
-            vertexConsumer.vertex(matrix4f, width + 1, 0, -extruder).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, width + 1, height + 1, -extruder).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 0, height + 1, -extruder).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 0, 0, -extruder).color(red, green, blue, alpha).endVertex();
-        }
-
-        //South
-        vertexConsumer.vertex(matrix4f, width + 1, 0, depth + 1 + extruder).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, width + 1, height + 1, depth + 1 + extruder).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, 0, height + 1, depth + 1 + extruder).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, 0, 0, depth + 1 + extruder).color(red, green, blue, alpha).endVertex();
-        if (addBackFaces) {
-            vertexConsumer.vertex(matrix4f, 0, 0, depth + 1 + extruder).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, 0, height + 1, depth + 1 + extruder).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, width + 1, height + 1, depth + 1 + extruder).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, width + 1, 0, depth + 1 + extruder).color(red, green, blue, alpha).endVertex();
-        }
-
-        //West
-        vertexConsumer.vertex(matrix4f, -extruder, 0, 0).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, -extruder, 0, depth + 1).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, -extruder, height + 1, depth + 1).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, -extruder, height + 1, 0).color(red, green, blue, alpha).endVertex();
-        if (addBackFaces) {
-            vertexConsumer.vertex(matrix4f, -extruder, height + 1, 0).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, -extruder, height + 1, depth + 1).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, -extruder, 0, depth + 1).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, -extruder, 0, 0).color(red, green, blue, alpha).endVertex();
-        }
-
-        //East
-        vertexConsumer.vertex(matrix4f, width + 1 + extruder, height + 1, 0).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, width + 1 + extruder, height + 1, depth + 1).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, width + 1 + extruder, 0, depth + 1).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix4f, width + 1 + extruder, 0, 0).color(red, green, blue, alpha).endVertex();
-        if (addBackFaces) {
-            vertexConsumer.vertex(matrix4f, width + 1 + extruder, 0, 0).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, width + 1 + extruder, 0, depth + 1).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, width + 1 + extruder, height + 1, depth + 1).color(red, green, blue, alpha).endVertex();
-            vertexConsumer.vertex(matrix4f, width + 1 + extruder, height + 1, 0).color(red, green, blue, alpha).endVertex();
-        }
-    }
 }
