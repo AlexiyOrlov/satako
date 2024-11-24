dad6c9ad96bf94f0189f03339af3514694aa9a8e
AlexiyOrlov <alexiy.ov@gmail.com>
2024-11-24 21:36:23 +0200

    Add optional background

--- src/main/java/dev/buildtool/satako/gui/Label.java
+++ src/main/java/dev/buildtool/satako/gui/Label.java
@@ -1,8 +1,8 @@
 package dev.buildtool.satako.gui;
 
+import dev.buildtool.satako.Constants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
@@ -13,21 +13,23 @@ import javax.annotation.Nullable;
  * Label is a string for use in GUIs
  */
 public class Label extends BetterButton implements Scrollable, Hideable {
-    protected boolean enabled, verticalScroll, horizontalScroll, hidden;
+    protected boolean enabled, verticalScroll, horizontalScroll, hidden,hasBackground;
     protected int scrollAmount;
     protected Screen parent;
 
     @SuppressWarnings("ConstantConditions")
-    public Label(int x, int y, Component text) {
+    public Label(int x, int y, Component text, boolean makeBackground) {
         super(x, y, Minecraft.getInstance().font.width(text.getString()) + 8, 18, text, null);
         scrollAmount = 20;
+        hasBackground=makeBackground;
     }
 
     @SuppressWarnings("ConstantConditions")
-    public Label(int x, int y, Component text, @Nullable Screen parent, @Nullable Button.OnPress pressHandler) {
+    public Label(int x, int y, Component text, @Nullable Screen parent, @Nullable OnPress pressHandler, boolean makeBackground) {
         super(x, y, Minecraft.getInstance().font.width(text.getString()) + 8, 18, text, pressHandler);
         scrollAmount = 20;
         this.parent = parent;
+        hasBackground=makeBackground;
     }
 
     @Override
@@ -39,11 +41,13 @@ public class Label extends BetterButton implements Scrollable, Hideable {
     @Override
     public void renderWidget(GuiGraphics guiGraphics, int p_renderButton_1_, int p_renderButton_2_, float p_renderButton_3_) {
         if (!hidden) {
+            if(hasBackground)
+            {
+                guiGraphics.fill(getX(),getY(),getX()+width,getY()+height, Constants.ORANGE.getIntColor());
+            }
             if (onPress != null) {
-                guiGraphics.drawString(Minecraft.getInstance().font, getMessage(), getX(), getY() + (height - 8) / 2, 16777215 | Mth.ceil(this.alpha * 255.0F) << 24);
-
-            } else
-                guiGraphics.drawString(Minecraft.getInstance().font, getMessage(), getX(), getY() + (height - 8) / 2, 16777215 | Mth.ceil(this.alpha * 255.0F) << 24);
+                guiGraphics.drawString(Minecraft.getInstance().font, getMessage(), getX()+4, getY() + (height - 8) / 2, 16777215 | Mth.ceil(this.alpha * 255.0F) << 24);
+            }
         }
     }
 
