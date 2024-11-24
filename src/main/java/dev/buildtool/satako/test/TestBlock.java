7c528cae3d7284e9c66c9a37c131cd65cf692643
AlexiyOrlov <alexiy.ov@gmail.com>
2024-11-24 11:51:33 +0200

    Try Tau

--- src/main/java/dev/buildtool/satako/test/TestBlock.java
+++ src/main/java/dev/buildtool/satako/test/TestBlock.java
@@ -1,6 +1,8 @@
 package dev.buildtool.satako.test;
 
+import com.github.wintersteve25.tau.renderer.ScreenUIRenderer;
 import dev.buildtool.satako.blocks.Block2;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
@@ -14,6 +16,9 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.network.NetworkHooks;
 
 import javax.annotation.Nullable;
@@ -25,8 +30,10 @@ public class TestBlock extends Block2 implements MenuProvider {
 
     @Override
     public InteractionResult use(BlockState p_225533_1_, Level world, BlockPos p_225533_3_, Player playerEntity, InteractionHand p_225533_5_, BlockHitResult p_225533_6_) {
-        if (playerEntity instanceof ServerPlayer)
-            NetworkHooks.openScreen((ServerPlayer) playerEntity, this);
+//        if (playerEntity instanceof ServerPlayer)
+//            NetworkHooks.openScreen((ServerPlayer) playerEntity, this);
+        if(world.isClientSide)
+            screen();
         return InteractionResult.SUCCESS;
     }
 
@@ -39,5 +46,11 @@ public class TestBlock extends Block2 implements MenuProvider {
     @Override
     public AbstractContainerMenu createMenu(int p_createMenu_1_, Inventory playerInventory, Player playerEntity) {
         return new TestContainer(p_createMenu_1_, playerInventory);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private void screen()
+    {
+        Minecraft.getInstance().setScreen(new ScreenUIRenderer(new TestTauScreen()));
     }
 }
---
+++ src/main/java/dev/buildtool/satako/test/TestTauScreen.java
@@ -0,0 +1,21 @@
+package dev.buildtool.satako.test;
+
+import com.github.wintersteve25.tau.components.base.UIComponent;
+import com.github.wintersteve25.tau.components.interactable.Button;
+import com.github.wintersteve25.tau.components.interactable.ListView;
+import com.github.wintersteve25.tau.components.layout.Stack;
+import com.github.wintersteve25.tau.components.utils.Container;
+import com.github.wintersteve25.tau.components.utils.Sized;
+import com.github.wintersteve25.tau.components.utils.Text;
+import com.github.wintersteve25.tau.layout.Layout;
+import com.github.wintersteve25.tau.theme.Theme;
+import com.github.wintersteve25.tau.utils.Size;
+
+import java.util.List;
+
+public class TestTauScreen implements UIComponent {
+    @Override
+    public UIComponent build(Layout layout, Theme theme) {
+        return new Sized(Size.staticSize(200,400),new Container.Builder().withChild(new ListView.Builder().build(new Button.Builder().build(new Text.Builder("1")),new Text.Builder("2"))));
+    }
+}
