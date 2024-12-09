522c3e48eadb7e101a33de335575aaf1d46f9a9c
AlexiyOrlov <alexiy.ov@gmail.com>
2024-12-09 09:56:56 +0200

    Tooltips

--- src/main/java/dev/buildtool/satako/gui/Screen2.java
+++ src/main/java/dev/buildtool/satako/gui/Screen2.java
@@ -4,6 +4,7 @@ import dev.buildtool.satako.Constants;
 import dev.buildtool.satako.IntegerColor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
@@ -22,6 +23,7 @@ public class Screen2 extends Screen
     protected int showTime=200;
     protected int popupPositionX, popupPositionY;
     protected LinkedHashMap<Component,Integer> showTimes=new LinkedHashMap<>();
+    protected HashMap<AbstractWidget,DynamicTooltip> tooltips=new HashMap<>();
 
     /**
      * GUI's center coordinates
@@ -49,6 +51,12 @@ public class Screen2 extends Screen
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float tick) {
         renderBackground(guiGraphics,mouseX,mouseY,tick);
         super.render(guiGraphics, mouseX, mouseY, tick);
+        tooltips.forEach((widget, tooltip) -> {
+            if(widget.getX()<mouseX && widget.getX()+widget.getWidth()>mouseX && mouseY>widget.getY() && mouseY<widget.getY()+widget.getHeight())
+            {
+                guiGraphics.renderTooltip(font,tooltip.getTooltip(),mouseX,mouseY);
+            }
+        });
         int popupY = popupPositionY - (showTimes.keySet().size()-1) * 18;
         for (Map.Entry<Component, Integer> entry : showTimes.entrySet()) {
             Component component = entry.getKey();
@@ -123,5 +131,10 @@ public class Screen2 extends Screen
     public void addPopup(Component message)
     {
         addPopup(message,showTime);
+    }
+
+    public void addTooltip(AbstractWidget target,DynamicTooltip tooltip)
+    {
+        tooltips.put(target,tooltip);
     }
 }
