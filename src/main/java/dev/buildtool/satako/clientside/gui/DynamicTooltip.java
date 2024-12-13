262d081089411ee8511a4c0650fad6e5281a56f3
AlexiyOrlov <alexiy.ov@gmail.com>
2024-12-13 18:01:19 +0200

    --

--- src/main/java/dev/buildtool/satako/clientside/gui/DynamicTooltip.java
+++ src/main/java/dev/buildtool/satako/clientside/gui/DynamicTooltip.java
@@ -1,7 +1,7 @@
 package dev.buildtool.satako.clientside.gui;
 
 import net.minecraft.network.chat.Component;
-
+@FunctionalInterface
 public interface DynamicTooltip {
     Component getTooltip();
 }
