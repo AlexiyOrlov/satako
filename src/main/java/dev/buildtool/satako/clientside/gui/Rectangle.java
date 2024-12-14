3b69fd4b11414fcc4ea73d4cb107962897b4880d
AlexiyOrlov <alexiy.ov@gmail.com>
2024-12-14 14:58:40 +0200

    Fix

--- src/main/java/dev/buildtool/satako/clientside/gui/Rectangle.java
+++ src/main/java/dev/buildtool/satako/clientside/gui/Rectangle.java
@@ -11,17 +11,13 @@ import javax.annotation.Nullable;
 
 public class Rectangle extends AbstractWidget {
     @Nullable
-    private  IntegerColor color;
-    private  FillPercent fillPercent;
+    private final IntegerColor color;
+    private final FillPercent fillPercent;
     private TextureAtlasSprite sprite;
     public Rectangle(int x, int y, int width, int height, @org.jetbrains.annotations.Nullable IntegerColor integerColor, @Nullable FillPercent fillPercent) {
         super(x, y, width, height, Component.literal(""));
         color=integerColor;
         this.fillPercent=fillPercent;
-    }
-
-    public Rectangle(int x, int y, int width, int height, Component message, @Nullable IntegerColor color) {
-        this(x, y, width, height,color,null);
     }
 
     public Rectangle(int x, int y, int width, int height, TextureAtlasSprite atlasSprite, @Nullable FillPercent fillPercent)
@@ -35,20 +31,41 @@ public class Rectangle extends AbstractWidget {
         this(x,y,width,height,atlasSprite,null);
     }
 
+    public Rectangle(int x, int y, int width, int height, @Nullable IntegerColor color, TextureAtlasSprite sprite, FillPercent fillPercent) {
+        super(x, y, width, height, Component.literal(""));
+        this.color = color;
+        this.fillPercent = fillPercent;
+        this.sprite = sprite;
+    }
+
+    public Rectangle(int x, int y, int width, int height, @Nullable IntegerColor color, TextureAtlasSprite atlasSprite)
+    {
+        this(x, y, width, height, color,atlasSprite,null);
+        this.sprite=atlasSprite;
+    }
+
     @Override
     protected void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         guiGraphics.pose().translate(0,0,399);
-        if(color!=null) {
+        if(sprite!=null)
+        {
+            if(color!=null)
+                guiGraphics.setColor(color.getRed(),color.getGreen(),color.getBlue(),color.getAlpha());
+            if(fillPercent!=null)
+            {
+                guiGraphics.blit(getX(), (int) (getY()+ height-height*fillPercent.getFillPercent()),-90,width,(int) (height*fillPercent.getFillPercent()),sprite);
+            }
+            else
+            {
+                guiGraphics.blit(getX(),getY(),-90,width,height,sprite);
+            }
+        }
+        else  {
             if (fillPercent != null) {
                 guiGraphics.fill(getX(), (int) (getY() + height - height * fillPercent.getFillPercent()), getX() + width, getY() + height, color.getIntColor());
             } else {
                 guiGraphics.fill(getX(), getY(), getX() + width, getY() + height, color.getIntColor());
             }
-        } else if (sprite != null) {
-            if(fillPercent!=null)
-                guiGraphics.blit(getX(), (int) (getY()+ height-height*fillPercent.getFillPercent()),-90,width,(int) (height*fillPercent.getFillPercent()),sprite);
-            else
-                guiGraphics.blit(getX(),getY(),-90,width,height,sprite);
         }
     }
 
