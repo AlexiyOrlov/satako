92190f077af6c48669ce12ea235d40201a1f6639
AlexiyOrlov <alexiy.ov@gmail.com>
2024-12-17 06:41:05 +0200

    Replicating

---
+++ src/main/java/dev/buildtool/satako/clientside/gui/ScrollBar.java
@@ -0,0 +1,187 @@
+package dev.buildtool.satako.clientside.gui;
+
+import dev.ftb.mods.ftblibrary.math.MathUtils;
+import dev.ftb.mods.ftblibrary.ui.Theme;
+import dev.ftb.mods.ftblibrary.ui.WidgetType;
+import dev.ftb.mods.ftblibrary.ui.input.MouseButton;
+import dev.ftb.mods.ftblibrary.util.TooltipList;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.PlainTextContents;
+import net.minecraft.util.Mth;
+
+import static dev.ftb.mods.ftblibrary.ui.Widget.isMouseButtonDown;
+import static dev.ftb.mods.ftblibrary.ui.Widget.isShiftKeyDown;
+
+public class ScrollBar extends AbstractWidget {
+    private final int scrollBarSize;
+    private double value = 0.0;
+    private double scrollStep = 20.0;
+    private double grab = -10000.0;
+    private double minValue = 0.0;
+    private double maxValue = 100.0;
+    private boolean canAlwaysScroll = false;
+    private boolean canAlwaysScrollPlane = true;
+    protected final Plane plane;
+
+    public ScrollBar(int x, int y, int width, int height, Component message,int size,Plane plane) {
+        super(x, y, width, height, message);
+        scrollBarSize=size;
+        this.plane=plane;
+    }
+
+    @Override
+    protected void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        int scrollBarSize = this.getScrollBarSize();
+        if (scrollBarSize > 0) {
+            double v = this.getValue();
+            if (this.grab != -10000.0) {
+                if (isMouseButtonDown(MouseButton.LEFT)) {
+                    if (this.plane.isVertical) {
+                        v = ((double)mouseY - ((double)getY() + this.grab)) * this.getMaxValue() / (double)(this.height - scrollBarSize);
+                    } else {
+                        v = ((double)mouseX - ((double)getX() + this.grab)) * this.getMaxValue() / (double)(this.width - scrollBarSize);
+                    }
+                } else {
+                    this.grab = -10000.0;
+                }
+            }
+
+            this.setValue(v);
+        }
+
+        this.drawBackground(guiGraphics,  getX(), getY(), this.width, this.height);
+        if (scrollBarSize > 0) {
+            if (this.plane.isVertical) {
+                this.drawScrollBar(guiGraphics, getX(), getY() + this.getMappedValue(this.height - scrollBarSize), this.width, scrollBarSize);
+            } else {
+                this.drawScrollBar(guiGraphics, getX() + this.getMappedValue((this.width - scrollBarSize)), getY(), scrollBarSize, this.height);
+            }
+        }
+    }
+
+    @Override
+    protected void updateWidgetNarration(NarrationElementOutput narrationElementOutput) {
+
+    }
+
+    public void setCanAlwaysScroll(boolean v) {
+        this.canAlwaysScroll = v;
+    }
+
+    public void setCanAlwaysScrollPlane(boolean v) {
+        this.canAlwaysScrollPlane = v;
+    }
+
+    public Plane getPlane() {
+        return this.plane;
+    }
+
+    public double getMinValue() {
+        return this.minValue;
+    }
+
+    public void setMinValue(double min) {
+        this.minValue = min;
+        this.setValue(this.getValue());
+    }
+
+    public double getMaxValue() {
+        return this.maxValue;
+    }
+
+    public void setMaxValue(double max) {
+        this.maxValue = max;
+        this.setValue(this.getValue());
+    }
+
+    public int getScrollBarSize() {
+        return this.scrollBarSize;
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (this.isMouseOver(mouseX,mouseY)) {
+            this.grab = this.plane.isVertical ? (double)mouseY - ((double)this.getY() + this.getMappedValue((double)(this.height - this.getScrollBarSize()))) : (double)mouseX - ((double)this.getX() + this.getMappedValue((double)(this.width - this.getScrollBarSize())));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double scrollX, double scrollY) {
+        if (scrollY != 0.0 && this.canMouseScrollPlane() && this.canMouseScroll(mouseX,mouseY)) {
+            this.setValue(this.getValue() - this.getScrollStep() * scrollY);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void drawBackground(GuiGraphics graphics, int x, int y, int w, int h) {
+
+    }
+
+    public void drawScrollBar(GuiGraphics graphics, int x, int y, int w, int h) {
+        //TODO
+//        theme.drawScrollBar(graphics, x, y, w, h, WidgetType.mouseOver(this.grab != -10000.0), this.plane.isVertical);
+    }
+
+    public void onMoved() {
+    }
+
+    public boolean canMouseScrollPlane() {
+        return this.canAlwaysScrollPlane || isShiftKeyDown() != this.plane.isVertical;
+    }
+
+    public boolean canMouseScroll(double mx,double my) {
+        return this.canAlwaysScroll || this.isMouseOver(mx,my);
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public void setValue(double v) {
+        v = Mth.clamp(v, this.getMinValue(), this.getMaxValue());
+        if (this.value != v) {
+            this.value = v;
+            this.onMoved();
+        }
+
+    }
+
+    public int getMappedValue(double max) {
+        return (int) MathUtils.map(this.getMinValue(), this.getMaxValue(), 0.0, max, this.value);
+    }
+
+    public double getScrollStep() {
+        return this.scrollStep;
+    }
+
+    public void setScrollStep(double s) {
+        this.scrollStep = Math.max(0.0, s);
+    }
+
+    public static enum Plane {
+        HORIZONTAL(false),
+        VERTICAL(true);
+
+        public final boolean isVertical;
+
+        private Plane(boolean v) {
+            this.isVertical = v;
+        }
+    }
+
+    public  static class PanelScrollBar extends ScrollBar{
+
+        public PanelScrollBar(int x, int y, int width, int height, Component message, int size, Plane plane) {
+            super(x, y, width, height, message, size, plane);
+        }
+    }
+}
--- src/main/java/dev/buildtool/satako/clientside/gui/ScrollPane.java
+++ src/main/java/dev/buildtool/satako/clientside/gui/ScrollPane.java
@@ -3,9 +3,11 @@ package dev.buildtool.satako.clientside.gui;
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.buildtool.satako.Constants;
 import dev.buildtool.satako.IntegerColor;
+import dev.ftb.mods.ftblibrary.ui.Widget;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.sounds.SoundManager;
@@ -14,90 +16,167 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import static dev.ftb.mods.ftblibrary.ui.Widget.isShiftKeyDown;
+
 @Deprecated
 public class ScrollPane extends AbstractWidget {
-    public static final int SCROLL_BAR_HEIGHT = 27;
-    public static final int SCROLL_BAR_WIDTH = 6;
-    protected AbstractContainerMenu menu;
-//    private static final ResourceLocation VILLAGER_LOCATION = new ResourceLocation("textures/gui/container/villager2.png");
-    private static final IntegerColor backgroundColor = new IntegerColor(0x4466FC5e), barColor = new IntegerColor(0xE34063ff);
-    int scrollOff;
-    protected int topPos;
-    protected boolean isDragging;
-    protected List<AbstractWidget> guiListeners;
-    protected AbstractContainerScreen<?> owner;
-
-    public ScrollPane(int x, int y, int width, int height, Component label, AbstractContainerScreen<?> owner,AbstractWidget... widgets) {
-        super(x, y, width, height, label);
-        menu=owner.getMenu();
-        topPos=owner.getGuiTop();
-        guiListeners=List.of(widgets);
-        this.owner=owner;
-        guiListeners.forEach(pWidget -> {
-            pWidget.setY(pWidget.getY()+getY());
-//            owner.addRenderableWidget(pWidget);
-        });
-    }
-
-    public ScrollPane(int x,int y,int width,int height,Component label,AbstractContainerScreen<?> owner,List<AbstractWidget> abstractWidgets)
-    {
-        this(x,y,width,height,label,owner,abstractWidgets.toArray(AbstractWidget[]::new));
+    private double scrollX = 0.0;
+    private double scrollY = 0.0;
+    private int contentWidth = -1;
+    private int contentHeight = -1;
+    private int contentWidthExtra;
+    private int contentHeightExtra;
+    private int offsetX = 0;
+    private int offsetY = 0;
+    private List<GuiEventListener> widgets=new ArrayList<>();
+    private ScrollBar attachedScrollbar;
+    private int scrollStep=20;
+    public ScrollPane(int x, int y, int width, int height, Component message) {
+        super(x, y, width, height, message);
     }
 
     @Override
-    protected void renderWidget(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+    protected void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
 
     }
 
     @Override
-    protected void updateWidgetNarration(NarrationElementOutput p_259858_) {
+    protected void updateWidgetNarration(NarrationElementOutput narrationElementOutput) {
 
     }
 
-    @Override
-    public void playDownSound(SoundManager p_93665_) {
-
+    public double getScrollX() {
+        return scrollX;
     }
 
-    @Override
-    public boolean mouseDragged(double pMouseX, double pMouseY, int pButton, double pDragX, double pDragY) {
-        if (this.isDragging) {
-            scrollOff= (int) Mth.clamp(pMouseY-height,0,height-SCROLL_BAR_HEIGHT);
-            for (AbstractWidget guiListener : guiListeners) {
-                guiListener.setY((int) (guiListener.getY()-20*Math.signum(pDragY)));
+    public void setScrollX(double scroll) {
+        this.scrollX = scroll;
+    }
+
+    public double getScrollY() {
+        return this.scrollY;
+    }
+
+    public void setScrollY(double scrollY) {
+        this.scrollY = scrollY;
+    }
+
+    public int getContentWidth() {
+        if (this.contentWidth == -1) {
+            int minX = Integer.MAX_VALUE;
+            int maxX = Integer.MIN_VALUE;
+            Iterator var3 = this.widgets.iterator();
+
+            while(var3.hasNext()) {
+                Widget widget = (Widget)var3.next();
+                if (widget.posX < minX) {
+                    minX = widget.posX;
+                }
+
+                if (widget.posX + widget.width > maxX) {
+                    maxX = widget.posX + widget.width;
+                }
             }
-            return true;
+
+            this.contentWidth = maxX - minX + this.contentWidthExtra;
+        }
+
+        return this.contentWidth;
+    }
+
+    public int getContentHeight() {
+        if (this.contentHeight == -1) {
+            int minY = Integer.MAX_VALUE;
+            int maxY = Integer.MIN_VALUE;
+            Iterator var3 = this.widgets.iterator();
+
+            while(var3.hasNext()) {
+                Widget widget = (Widget)var3.next();
+                if (widget.posY < minY) {
+                    minY = widget.posY;
+                }
+
+                if (widget.posY + widget.height > maxY) {
+                    maxY = widget.posY + widget.height;
+                }
+            }
+
+            this.contentHeight = maxY - minY + this.contentHeightExtra;
+        }
+
+        return this.contentHeight;
+    }
+
+    public void setOffset(boolean flag) {
+        if (flag) {
+            this.offsetX = (int)(-this.scrollX);
+            this.offsetY = (int)(-this.scrollY);
         } else {
-            return super.mouseDragged(pMouseX, pMouseY, pButton, pDragX, pDragY);
+            this.offsetX = this.offsetY = 0;
+        }
+
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double scrollX, double scrollY) {
+        this.setOffset(true);
+
+        for(int i = this.widgets.size() - 1; i >= 0; --i) {
+            Widget widget = (Widget)this.widgets.get(i);
+            if (widget.isEnabled() && widget.mouseScrolled(scrollY)) {
+                this.setOffset(false);
+                return true;
+            }
+        }
+
+        boolean scrollPanel = this.scrollPanel(scrollY,mouseX,mouseY);
+        this.setOffset(false);
+        return scrollPanel;
+    }
+
+    public double getScrollStep() {
+        return this.scrollStep;
+    }
+
+    public boolean scrollPanel(double scroll, double mouseX, double mouseY) {
+        if (this.attachedScrollbar == null && this.isMouseOver(mouseX,mouseY)) {
+            return this.isDefaultScrollVertical() != isShiftKeyDown() ? this.movePanelScroll(0.0, -this.getScrollStep() * scroll) : this.movePanelScroll(-this.getScrollStep() * scroll, 0.0);
+        } else {
+            return false;
         }
     }
 
-    public boolean mouseClicked(double pMouseX, double pMouseY, int pButton) {
-        this.isDragging = !guiListeners.isEmpty() && pMouseX > getX() + width && pMouseX < getX() + width + SCROLL_BAR_WIDTH && pMouseY > getY() && pMouseY < getY() + height;
-        return super.mouseClicked(pMouseX, pMouseY, pButton);
+    boolean isDefaultScrollVertical()
+    {
+        return true;
     }
 
-    private void renderScroller(GuiGraphics pGuiGraphics, int pPosX, int pPosY) {
-        if (!guiListeners.isEmpty()) {
-//            pGuiGraphics.blit(VILLAGER_LOCATION, getX()+width, getY() + scrollOff, 0, 0.0F, 199.0F, SCROLL_BAR_WIDTH, SCROLL_BAR_HEIGHT, 512, 256);
+    public boolean movePanelScroll(double dx, double dy) {
+        if (dx == 0.0 && dy == 0.0) {
+            return false;
+        } else {
+            double sx = this.getScrollX();
+            double sy = this.getScrollY();
+            int h;
+            if (dx != 0.0) {
+                h = this.getContentWidth();
+                if (h > this.width) {
+                    this.setScrollX(Mth.clamp(sx + dx, 0.0, (double)(h - this.width)));
+                }
+            }
+
+            if (dy != 0.0) {
+                h = this.getContentHeight();
+                if (h > this.height) {
+                    this.setScrollY(Mth.clamp(sy + dy, 0.0, (double)(h - this.height)));
+                }
+            }
+
+            return this.getScrollX() != sx || this.getScrollY() != sy;
         }
     }
-
-//    @Override
-//    public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
-//        pGuiGraphics.drawString(Minecraft.getInstance().font, getMessage(),getX()+width/2-Minecraft.getInstance().font.width(getMessage())/2,getY()-12,0xE35F3B);
-//        if (owner.getMinecraft().level != null) {
-//            pGuiGraphics.fillGradient(getX(), getY(),getX()+ this.width,getY()+ this.height, Constants.GREEN.getIntColor(), Constants.GREEN.getIntColor());
-//        } else {
-//            owner.renderDirtBackground(pGuiGraphics);
-//        }
-//        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
-//        if (!guiListeners.isEmpty()) {
-//            int i = this.width;
-//            int j = this.height;
-//            this.renderScroller(pGuiGraphics, i, j);
-//            RenderSystem.enableDepthTest();
-//        }
-//    }
 }
