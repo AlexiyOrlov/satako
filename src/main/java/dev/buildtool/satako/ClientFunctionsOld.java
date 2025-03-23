7a76c66f756002c65f61aac5a3ec7c8669d3a3da
AlexiyOrlov <alexiy.ov@gmail.com>
2025-03-23 14:00:40 +0200

    --

--- src/main/java/dev/buildtool/satako/ClientFunctionsOld.java
+++ src/main/java/dev/buildtool/satako/ClientFunctionsOld.java
@@ -13,7 +13,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class ClientFunctionsOld {
     public static VertexConsumer createTransclucentStateBuffer(MultiBufferSource bufferSource) {
-        return bufferSource.getBuffer(RenderType.create("opaque", DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.QUADS, 256, false, false, ClientConstants.translucentCompositeState));
+        return null;//bufferSource.getBuffer(RenderType.create("opaque", DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.QUADS, 256, false, false, ClientConstants.translucentCompositeState));
     }
 
     public static int calculateStringWidth(Component string) {
