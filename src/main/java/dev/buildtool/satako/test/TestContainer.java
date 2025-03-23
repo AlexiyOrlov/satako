b2aa285c22049e5b73147f31d17e38b261154f93
AlexiyOrlov <alexiy.ov@gmail.com>
2025-03-23 14:01:24 +0200

    --

--- src/main/java/dev/buildtool/satako/test/TestContainer.java
+++ src/main/java/dev/buildtool/satako/test/TestContainer.java
@@ -15,8 +15,8 @@ public class TestContainer extends Container2 {
     public TestContainer(int i, Inventory playerInventory) {
         super(Satako.TEST_CONTAINER.get(), i);
         ItemHandler itemHandler = new ItemHandler(2);
-        addSlot(new ItemHandlerDisplaySlot(itemHandler, 0, 0, 0).setColor(new IntegerColor(0xff4f6a7b)).setTooltip(List.of(Component.literal("Tooltip line 1"),Component.literal("Tooltip line 2"))));
-        addSlot(new ItemHandlerSlot(itemHandler, 1, 18, 0).setColor(new IntegerColor(0xff4f6a7b)).setTooltip(List.of(Component.literal("Tooltip line 1"),Component.literal("Tooltip line 2"))));
+        addSlot(new ItemHandlerDisplaySlot(itemHandler, 0, 0, 0).setColor(new IntegerColor(0xff4f6a7b)).setTooltip(Component.literal("Tooltip line 1")));
+        addSlot(new ItemHandlerSlot(itemHandler, 1, 18, 0).setColor(new IntegerColor(0xff4f6a7b)).setTooltip(Component.literal("Tooltip line 1")));
         addPlayerInventory(0, 20, playerInventory);
     }
 }
