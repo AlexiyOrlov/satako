19c647a20679bb46fd295e6aa59f6f4ba4c9e1bd
AlexiyOrlov <alexiy.ov@gmail.com>
2026-01-14 15:03:29 +0200

    Add

--- common/src/main/java/dev/buildtool/satako/client/ClientConstants.java
+++ common/src/main/java/dev/buildtool/satako/client/ClientConstants.java
@@ -2,9 +2,11 @@ package dev.buildtool.satako.client;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import dev.buildtool.satako.Satako;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 
 public class ClientConstants {
     private static final RenderStateShard.TransparencyStateShard TRANSLUCENT_TRANSPARENCY = new RenderStateShard.TransparencyStateShard("translucent_transparency", () -> {
@@ -15,4 +17,13 @@ public class ClientConstants {
         RenderSystem.defaultBlendFunc();
     });
     static final RenderType.CompositeState translucentCompositeState = RenderType.CompositeState.builder().setTransparencyState(TRANSLUCENT_TRANSPARENCY).setWriteMaskState(new RenderStateShard.WriteMaskStateShard(true, true)).setShaderState(new RenderStateShard.ShaderStateShard(GameRenderer::getRendertypeLightningShader)).createCompositeState(false);
+
+    public static final ResourceLocation ORANGE_SQUARE = ResourceLocation.fromNamespaceAndPath(Satako.ID, "orange");
+    public static final ResourceLocation DARK_SQUARE = ResourceLocation.fromNamespaceAndPath(Satako.ID, "dark");
+    public static final ResourceLocation BLUE_SQUARE = ResourceLocation.fromNamespaceAndPath(Satako.ID, "blue");
+    public static final ResourceLocation GRAY_SQUARE = ResourceLocation.fromNamespaceAndPath(Satako.ID, "gray");
+    public static final ResourceLocation GREEN_SQUARE = ResourceLocation.fromNamespaceAndPath(Satako.ID, "green");
+    public static final ResourceLocation YELLOW_SQUARE=ResourceLocation.fromNamespaceAndPath(Satako.ID,"yellow");
+    public static final ResourceLocation WHITE_SQUARE=ResourceLocation.fromNamespaceAndPath(Satako.ID,"white");
+    public static final ResourceLocation BLACK_SQUARE=ResourceLocation.fromNamespaceAndPath(Satako.ID,"black");
 }
