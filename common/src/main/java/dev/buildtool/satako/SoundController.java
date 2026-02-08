c3f14f236400646528c4d7d5d812749d08a1ec06
AlexiyOrlov <alexiy.ov@gmail.com>
2026-02-08 23:22:52 +0200

    Should crash

--- common/src/main/java/dev/buildtool/satako/SoundController.java
+++ common/src/main/java/dev/buildtool/satako/SoundController.java
@@ -11,23 +11,19 @@ public class SoundController {
     private static final HashMap<BlockPos, SoundInstanceImpl> soundMap = new HashMap<>();
 
     public static void startBlockSound(SoundEvent sound, BlockPos pos, float volume) {
-        if(soundEngine!=null) {
-            SoundInstanceImpl soundInstance = soundMap.get(pos);
-            if (soundInstance == null || !soundEngine.isActive(soundInstance)) {
-                soundInstance = new SoundInstanceImpl(sound, pos, volume);
-                soundEngine.soundManager.play(soundInstance);
-                soundMap.put(pos, soundInstance);
-            }
+        SoundInstanceImpl soundInstance = soundMap.get(pos);
+        if (soundInstance == null || !soundEngine.isActive(soundInstance)) {
+            soundInstance = new SoundInstanceImpl(sound, pos, volume);
+            soundEngine.soundManager.play(soundInstance);
+            soundMap.put(pos, soundInstance);
         }
     }
 
     public static void stopBlockSound(BlockPos pos) {
-        if(soundEngine!=null) {
-            SoundInstanceImpl existing = soundMap.get(pos);
-            if (existing != null) {
-                soundEngine.soundManager.stop(existing);
-                soundMap.remove(pos);
-            }
+        SoundInstanceImpl existing = soundMap.get(pos);
+        if (existing != null) {
+            soundEngine.soundManager.stop(existing);
+            soundMap.remove(pos);
         }
     }
 }
